6952ee887cde84950041fbacb76f5241bd31d893
Federico Carballo <ganimedes2q@gmail.com>
2022-08-10 19:31:14 -0300

    Cumplo con la consigna

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Navbar from './components/Navbar.jsx/Navbar';
 import './App.css';
+import Carditems from './components/Cards.jsx/Cardsitems';
 
 
 
 function App() {
   return (
-  <Navbar/>
+    <>
+      <Navbar/>
+      <Carditems />
+    </>
   );
 }
 
--- src/components/Navbar.jsx/Navbar.jsx
+++ src/components/Navbar.jsx/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import Cartwidget from "../Cartwidget.jsx/Cartwidget";
 import "./Navbar.css";
 import logo from "./scarlettlogo.png";
+import '../Cartwidget.jsx/cartwidget.png'
 
 function Navbar() {
+
+  const navBarItems = ["Tienda +", "Nosotros", "Contacto", "Registrate", <Cartwidget />];
+  window.addEventListener("scroll", function(){
+    let header = document.querySelector("header");
+    header.classList.toggle("abajo", window.scrollY>0);
+  })
+
   return (
     <>
       <header>
@@ -12,24 +21,20 @@ function Navbar() {
           </a>
           <div className="menu">
             <ul className="list">
-              <li>
-                <a href="/">Tienda +</a>
-              </li>
-              <li>
-                <a href="/">Nosotros</a>
-              </li>
-              <li>
-                <a href="/">Contacto</a>
-              </li>
-              <li>
-                <a href="/">Registrate</a>
-              </li>
-              <li><a href="/">Tus compras: $0</a></li>
+            {navBarItems.map((item) => (
+              <li><a href="/">{item}</a></li>
+            ))}
             </ul>
           </div>
         </nav>
       </header>
       <section className="zona1"></section>
+
+      <div className="titulo-tortas">
+      <h1>Tortas</h1>
+      <hr className="hr-tortas" />
+      </div>
+      
     </>
   );
 }
